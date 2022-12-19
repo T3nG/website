345d98a737d309671cbef95480a75f0e754b8582
T3nG <dengfixanros@gmail.com>
2022-12-20 05:28:09 +0800

    semi-finished, security issue not solved yet: seek more secure method than session, prevent direct url link to php pages.

--- php/connection.php
+++ php/connection.php
@@ -19,7 +19,7 @@ define('DB_DATABASE',
 $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 if($conn->connect_error){
     die('Error connected');
-    }
+}
 else{
     return $conn;
 }
